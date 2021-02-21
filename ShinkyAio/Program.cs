211102eb18eb1a5ccfdea8bc8f1e49d4d0049840
Shinky <79386052+ShinkyCode@users.noreply.github.com>
2021-02-21 22:01:51 +0100

    Add files via upload

---
+++ ShinkyAio/Program.cs
@@ -0,0 +1,28 @@
+﻿using EnsoulSharp;
+using EnsoulSharp.SDK;
+using EnsoulSharp.SDK.MenuUI;
+
+namespace ShinkyAio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameEvent.OnGameLoad += OnLoadGame;
+        }
+
+        private static void OnLoadGame()
+        {
+            var pred = new Menu("spred", "Predict Selection", true);
+            SPrediction.Prediction.Initialize(pred);
+            pred.Attach();
+
+            new AutoLevelUp().Load();
+
+            if (ObjectManager.Player.CharacterName == "Blitzcrank")
+            {
+                new ShinkyBlitz().OnLoad();
+            }
+        }
+    }
+}
